de8dbb9bbc3f9419d75ade1d48f393662bc599cf
Suyog Sigdel <suyogsigdel000@gmail.com>
2026-09-05 21:26:04 +0545

    feat: connect dashboard to backend

--- src/pages/Dashboard/DashboardPage.tsx
+++ src/pages/Dashboard/DashboardPage.tsx
@@ -1,7 +1,13 @@
-// src/pages/DashboardPage.tsx
+import { useEffect } from "react";
 import { AppLayout } from "../components/layout/AppLayout";
 
 export const DashboardPage = () => {
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/`)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <AppLayout title="Dashboard">
       <div className="space-y-6">
